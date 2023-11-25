c4987dd1a7172ab426ad24cb9c38f87bcb8ed01c
CubeLov <1357157312@qq.com>
2023-11-25 17:08:29 +0800

    add new class: TestClass

---
+++ TestClass.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class TestClass {
+    public static void main(String[] args) {
+        int a=0;
+        int b=0;
+        Scanner sc=new Scanner(System.in);
+        int c=sc.nextInt();
+        System.out.println(a+b+c);
+    }
+}
